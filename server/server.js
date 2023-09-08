61b903a78a108dc1d4453b5decaa170508737792
Nicholas_Huang713 <nhuang713@gmail.com>
2023-09-08 09:34:37 -0700

    add cors policy

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 // server/server.js
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors')
+// const cors = require('cors')
 const users = require('./routes/user');
 const path = require('path');
 
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
   
 
-  app.use(cors());
+  // app.use(cors());
   app.use(express.json());
   
   mongoose.connect('mongodb://127.0.0.1:27017/img-board', {
@@ -19,6 +19,12 @@ const PORT = process.env.PORT || 5000;
     .catch(console.error);
 
 app.use('/users', users)
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 const _dirname = path.dirname("");
 const buildPath = path.join(_dirname, "../client/build")
