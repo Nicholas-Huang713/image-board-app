c4230c0143bc050ea5dddc8056dc5b056b613e2d
Nicholas_Huang713 <nhuang713@gmail.com>
2023-09-10 08:55:12 -0700

    update corsoptions

--- server/server.js
+++ server/server.js
@@ -8,11 +8,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
   
-// const corsOptions = {
-//   origin: 'http://54.153.95.6',
-// };
-// app.use(cors(corsOptions));
-app.use(cors());
+const corsOptions = {
+  origin: 'http://54.153.95.6',
+};
+app.use(cors(corsOptions));
+// app.use(cors());
 app.use(express.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/img-board', {
