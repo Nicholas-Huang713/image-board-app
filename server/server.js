5b5c85e99e129091a71b5fcf90293d2b112ac430
Nicholas_Huang713 <nhuang713@gmail.com>
2023-09-14 19:52:56 -0700

    update port

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const dotenv = require('dotenv');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
   
 app.use(cors());
 app.use(express.json());
